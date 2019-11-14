77bf7e80a91f57fd85db11f1e41100dab9cfaff6
Sam Heyman <sam.heyman@gmail.com>
2019-11-14 20:43:15 +0100

    First draft: navigation bar and homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Material UI imports
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+// Custom components
+import Navbar from './components/Navbar';
+
+// Define the app theme:
+const theme = createMuiTheme({
+  typography: {
+    h1: {
+      fontSize: '1.9rem',
+      fontWeight: 400,
+    },
+  },
+  palette: {
+    background: {
+      default: "#ffffff"
+    },
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#1c90f3',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      light: '#0066ff',
+      main: '#ff3860',
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: '#ffcc00',
+    },
+  },
+});
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Navbar />
+      <Container maxWidth="lg">
+        <Typography variant="h1">Homepage</Typography>
+      </Container>
+    </MuiThemeProvider>
   );
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import StarHalfIcon from '@material-ui/icons/StarHalf';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1, 
+    },
+    container: {
+      backgroundColor: 'white',
+      color: 'rgba(74,74,74,1)',
+      boxShadow: '0 0 4px rgba(0,0,0,.1),'
+               + '0 2px 4px rgba(0,0,0,.1);'
+    },
+    icon: {
+      fontSize: '1.8rem',
+      marginRight: '5px',
+      color: theme.palette.primary.main,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+      fontSize: '1.25rem',
+      fontWeight: 500,
+    },
+  }),
+);
+
+export default function MenuAppBar() {
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const signOut = () => {
+    setAuth(false);
+  }
+
+  return (
+    <div className={classes.root}>
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch color="default" checked={auth} onChange={handleChange} aria-label="login switch" />}
+          label={auth ? 'Logout' : 'Login'}
+        />
+      </FormGroup>
+      <AppBar position="static" className={classes.container}>
+        <Toolbar>
+          <StarHalfIcon className={classes.icon}/>
+          <Typography variant="h1" className={classes.title}>
+            WebStarter
+          </Typography>
+          <ul>
+            <li>Search</li>
+          </ul>
+          {auth && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={signOut}>Sign out</MenuItem>
+              </Menu>
+            </div>
+          )}  
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
